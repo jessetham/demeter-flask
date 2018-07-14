e99935a8a19cb1207903e905b456ad3c206f1d1c
Jesse Tham <jtham@ualberta.ca>
2018-07-14 14:43:53 -0600

    Added Model class
    Model class adds helper functions for all models in the db

--- app/models.py
+++ app/models.py
@@ -1,22 +1,38 @@
 from datetime import datetime
 from app import db
 
-class Sensor(db.Model):
+class Model:
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def get_all(cls):
+        return cls.query.all()
+
+class Sensor(db.Model, Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     readings = db.relationship('Reading', backref='publisher', lazy='dynamic')
 
+    def __init__(self, name):
+        self.name = name
+
     def __repr__(self):
         return '<Sensor {}>'.format(self.name)
 
-class Reading(db.Model):
+class Reading(db.Model, Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.Integer)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
 
-class Category(db.Model):
+class Category(db.Model, Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), index=True, unique=True)
     units = db.Column(db.String(16), unique=True)
