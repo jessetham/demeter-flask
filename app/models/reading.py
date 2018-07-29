9e11425ce75d2324335f62efe42b2e160eaef8bc
Jesse Tham <jtham@ualberta.ca>
2018-07-28 19:40:36 -0600

    Update from_dict() for reading model
    New methods to link reading to sensor and category from their names

--- app/models/reading.py
+++ app/models/reading.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 from app import db
+from app.models.category import Category
+from app.models.sensor import Sensor
 from app.models.mixins import ModelMixin
 
 class Reading(db.Model, ModelMixin):
@@ -9,8 +11,22 @@ class Reading(db.Model, ModelMixin):
     sensor_id = db.Column(db.Integer, db.ForeignKey('sensor.id'))
     category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
 
+    def add_category_to_reading(self, data):
+        if 'category' in data:
+            category = Category.query.filter_by(name=data['category']).first()
+            if category:
+                self.category = category
+
+    def add_sensor_to_reading(self, data):
+        if 'sensor' in data:
+            sensor = Sensor.query.filter_by(name=data['sensor']).first()
+            if sensor:
+                self.sensor = sensor
+
     def from_dict(self, data):
-        for field in ['data']:
+        self.add_category_to_reading(data)
+        self.add_sensor_to_reading(data)
+        for field in ['data', 'timestamp']:
             if field in data:
                 setattr(self, field, data[field])
 
