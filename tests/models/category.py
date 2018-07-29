ad2ea03d3bb49d36046f0749528296e33384e13b
Jesse Tham <jtham@ualberta.ca>
2018-07-28 20:21:10 -0600

    Add testing for to_dict() and from_dict() methods for category model

--- tests/models/category.py
+++ tests/models/category.py
@@ -15,3 +15,17 @@ class CategoryModelCase(BaseCase):
         # Test checking an array with one invalid category
         valid = Category.are_valid_categories(categories)
         self.assertEqual(valid, False)
+
+    def test_from_and_to_dict(self):
+        category = utl.CATEGORIES[-1]
+        c = Category()
+
+        # Test using the from_dict() method
+        c.from_dict(category)
+        self.assertEqual(c.name, category['name'])
+        self.assertEqual(c.units, category['units'])
+
+        # Test using the to_dict() method
+        c_dict = c.to_dict()
+        self.assertEqual(c_dict['name'], category['name'])
+        self.assertEqual(c_dict['units'], category['units'])
