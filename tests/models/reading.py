f001ecdb7202c5a352d1b67943b07333d67ddb69
Jesse Tham <jtham@ualberta.ca>
2018-08-03 20:03:26 -0600

    Add from_dict() and to_dict() for Reading model

--- tests/models/reading.py
+++ tests/models/reading.py
@@ -23,4 +23,28 @@ class ReadingModelCase(BaseCase):
         self.assertEqual(r.sensor.name, sensor['name'])
 
     def test_from_and_to_dict(self):
-        pass
+        utl.add_categories_to_db()
+        category = utl.CATEGORIES[-1]
+        utl.add_sensors_to_db()
+        sensor = utl.SENSORS[-1]
+        r = Reading()
+
+        reading = {
+            'data': 123,
+            'category': category['name'],
+            'sensor': sensor['name'],
+            'timestamp': 123,
+        }
+        # Test the from_dict() method
+        r.from_dict(reading)
+        self.assertEqual(r.data, reading['data'])
+        self.assertEqual(r.category.name, reading['category'])
+        self.assertEqual(r.sensor.name, reading['sensor'])
+        self.assertEqual(r.timestamp, reading['timestamp'])
+
+        # Test the to_dict() method
+        r_dict = r.to_dict()
+        self.assertEqual(r_dict['data'], reading['data'])
+        self.assertEqual(r_dict['category']['name'], reading['category'])
+        self.assertEqual(r_dict['sensor']['name'], reading['sensor'])
+        self.assertEqual(r_dict['timestamp'], reading['timestamp'])
