1ff617350cd9356224c6ed920c371cde2a007b05
Jesse Tham <jtham@ualberta.ca>
2018-08-14 20:39:19 -0600

    Reformat import statements

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,11 @@ from flask import Blueprint
 
 bp = Blueprint('api', __name__)
 
-from app.api import categories, errors, readings, sensors, users
+from app.api import (
+    categories,
+    errors,
+    readings,
+    sensors,
+    tokens,
+    users,
+)
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,3 +1,14 @@
 # The ordering is important, don't move these around
-from tests.models import CategoryModelCase, SensorModelCase, ReadingModelCase, UserModelCase
-from tests.api import CategoriesAPICase, SensorsAPICase, ReadingsAPICase, UsersAPICase
+from tests.models import (
+    CategoryModelCase,
+    SensorModelCase,
+    ReadingModelCase,
+    UserModelCase,
+)
+from tests.api import (
+    CategoriesAPICase,
+    SensorsAPICase,
+    TokensAPICase,
+    ReadingsAPICase,
+    UsersAPICase,
+)
